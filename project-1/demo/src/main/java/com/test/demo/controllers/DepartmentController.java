ba56326f6f2a9140f4825ac90492842a93319f51
Vitor H. Nolli <vitor.hugo@semexbrasil.local>
2023-10-17 18:53:16 -0300

    Find department and role by Id

--- project-1/demo/src/main/java/com/test/demo/controllers/DepartmentController.java
+++ project-1/demo/src/main/java/com/test/demo/controllers/DepartmentController.java
@@ -4,11 +4,13 @@ import com.test.demo.dtos.DepartmentDTO;
 import com.test.demo.dtos.DepartmentInsertDTO;
 import com.test.demo.entities.Department;
 import com.test.demo.repositories.DepartmentRepository;
+import com.test.demo.services.exceptionsTypes.ResourcesNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/department")
@@ -22,7 +24,13 @@ public class DepartmentController {
         List<Department> result = repository.findAll();
         return result;
     }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartmentDTO> findDepartmentById(@PathVariable Long id){
+        Optional<Department> entity = repository.findById(id);
+        Department department = entity.orElseThrow(()-> new ResourcesNotFound("Id " + id + " not found!"));
 
+        return ResponseEntity.ok().body(new DepartmentDTO(department.getId(), department.getName()));
+    }
     @PostMapping
     public ResponseEntity<DepartmentDTO> insert(@RequestBody DepartmentInsertDTO dto){
         Department department = new Department();
