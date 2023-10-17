9795369c551cc18861c14a9cbb0c7b9b82d230cd
Vitor H. Nolli <vitor.hugo@semexbrasil.local>
2023-10-17 19:10:36 -0300

    Insert User FrontEnd

--- project-1/front/js/employer/registerEmployer.js
+++ project-1/front/js/employer/registerEmployer.js
@@ -1,4 +1,4 @@
-function registerEmployer(name, email, departmentRoleId){
+function registerEmployer(name, email, role, department){
 
       $.ajax({
             url: "http://localhost:8080/users",
@@ -8,7 +8,12 @@ function registerEmployer(name, email, departmentRoleId){
             data: JSON.stringify({
                 name: name,
                 email: email,
-                departmentRole: departmentRoleId
+                role:{
+                    id: role
+                },
+                department:{
+                    id: department
+                }
             }),
             success: function(response){
                 $('#modal-register').modal('hide');
@@ -24,17 +29,4 @@ function registerEmployer(name, email, departmentRoleId){
                 console.log("Erro ao inserir: " + response);
             }
       })
-}
-function validateDepartmentRole(name, email, role, department){
-    $.ajax({
-        url: "http://localhost:8080/department-roles/department="+department+"/role="+role,
-        method: "GET",
-        dataType: "JSON",
-        success: function(response){
-            registerEmployer(name, email, response.id);
-        },
-        error: function(response){
-            alert("Erro ao cadastrar usuário com esse departamento e esse cargo " + response);
-        }
-    })
 }
